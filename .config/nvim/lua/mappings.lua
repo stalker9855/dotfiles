7e62be87d99bdf546f26fdb14fd8fe261cd8ed2d
stalker9855 <andrey.l.evseev@gmail.com>
2024-11-15 16:41:38 +0000

    dotfiles

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,5 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "window left" })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "window right" })
+map("n", "<C-j>", "<cmd>TmuxNavigatDown<CR>", { desc = "window down" })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "window up" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -41,4 +41,9 @@ return {
       }
     end,
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  { "catppuccin/nvim", name = "catppuccin"},
 }
