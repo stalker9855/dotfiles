41ab01ea760d28748beae0613d9814bc64100184
stalker9855 <andrey.l.evseev@gmail.com>
2025-02-21 11:39:52 +0200

    i3

---
+++ .config/polybar/i3_polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Завершить текущие экземпляры polybar
+killall -q polybar
+
+# Ожидание полного завершения работы процессов
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Запуск Polybar со стандартным расположением конфигурационного файла в ~/.config/polybar/config
+polybar --config=~/.config/polybar/i3_polybar/config.ini &
