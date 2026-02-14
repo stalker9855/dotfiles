ce24baec3827ea47d22439e779663581b7b3bc13
stalker9855 <andrey.l.evseev@gmail.com>
2026-02-14 08:47:03 +0000

    vim

---
+++ .config/nvim/lua/bobross/plugins/evergarden.lua
@@ -0,0 +1,25 @@
+return {
+	"everviolet/nvim",
+	name = "evergarden",
+	priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+	opts = {
+		theme = {
+			variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+			accent = "green",
+		},
+		editor = {
+			transparent_background = false,
+			sign = { color = "none" },
+			float = {
+				color = "mantle",
+				solid_border = true,
+			},
+			completion = {
+				color = "surface0",
+			},
+		},
+	},
+	config = function()
+		-- vim.cmd.colorscheme("evergarden")
+	end,
+}
